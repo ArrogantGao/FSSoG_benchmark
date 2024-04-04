3944887b3e488e8bf041aa1768d93cf009c6fada
xzgao <xz.gao@connect.ust.hk>
2024-04-04 19:52:45 +0800

    revise

--- accuracy/test_for_manu/run_T0.sh
+++
@@ -1,9 +0,0 @@
-#! bin/bash
-
-data_file=data_for_manu/Acc_T0_short.csv
-
-rm -rf $data_file
-touch $data_file
-echo "uspara, error" >> $data_file
-
-julia --project=. test_for_manu/T0_Short.jl
--- accuracy/test_for_manu/run_T5_2.sh
+++
@@ -1,9 +0,0 @@
-#! bin/bash
-
-data_file=data_for_manu/Acc_T5_2_ThinCheb.csv
-
-rm -rf $data_file
-touch $data_file
-echo "preset,Rz,E_thin_k,E_direct_k,error_rel" >> $data_file
-
-julia --project=. test_for_manu/T5_2_ThinCheb.jl
