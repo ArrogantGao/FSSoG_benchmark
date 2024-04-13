a20ba5a8d53353a87843516629da13246e8f51bb
xzgao <xz.gao@connect.ust.hk>
2024-04-13 22:24:27 +0800

    update

--- manuscript/data_scripts/T0_total.jl
+++ manuscript/data_scripts/T0_total.jl
@@ -4,23 +4,23 @@ using CSV, DataFrames
 L0 = 20.0
 N_real0 = (36, 36, 36)
 
-w = (8, 8, 8)
+w = (18, 18, 18)
 β = 5.0 .* w
 extra_pad_ratio_intial = 8
-cheb_order = 10
+cheb_order = 16
 M_mid_initial = 15
 uspara = USeriesPara(6)
 eta = uspara.sw[M_mid_initial][1] / L0 + 0.0001
-N_grid = (2, 2, 15)
-Qs = 28
-Ql = 12
+N_grid = (4, 4, 24)
+Qs = 32
+Ql = 32
 r_c = 9.99
 Q_0 = 32
-Rz_0 = 16
+Rz_0 = 32
 
 for preset in 1:6
     uspara = USeriesPara(preset)
-    path = "./reference/cube/n_3164.jld2" 
+    path = "./reference/cube/n_1000.jld2" 
     @load path n_atoms L atoms info energy_ewald
 
     @show n_atoms, L, energy_ewald
