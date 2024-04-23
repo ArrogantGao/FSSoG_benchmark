bc19524394ab3a3b0d70b6b124b5338c87d1bbf9
Qi Zhou <zhouqi1729@sjtu.edu.cn>
2024-04-23 15:08:24 +0800

    add parameter settings for high density system

--- manuscript/data_scripts/T6_cube_total_6.jl
+++ manuscript/data_scripts/T6_cube_total_6.jl
@@ -2,24 +2,27 @@ using ExTinyMD, FastSpecSoG, EwaldSummations, JLD2
 using CSV, DataFrames
 
 L0 = 10.0
-N_real0 = (90, 90, 90)
+N_real0 = (94, 94, 94)
 
-w = (13, 13, 13)
+w = (7, 7, 7)
 β = 5.0 .* w
 target = 1.5
-extra_pad_ratio_intial = ((target-1.0)*N_real0[1]+6.8*(target-1.0)*w[1]+2.4*target-4.4)/(2*w[1]) 
+#extra_pad_ratio_intial = ((target-1.0)*N_real0[1]+6.8*(target-1.0)*w[1]+2.4*target-4.4)/(2*w[1])
+extra_pad_ratio_intial = 10
+lambda_true = (2*extra_pad_ratio_intial*w[1]+N_real0[1]+6.8*w[1]+4.4)/(N_real0[1]+6.8*w[1]+2.4)
+@show lambda_true
 cheb_order = 10
 preset = 6
 uspara = USeriesPara(preset, r_c=1.799918355128706)
-M_mid_initial = 15
+M_mid_initial = 18
 eta = uspara.sw[M_mid_initial][1] / L0 + 0.0001
 @show eta
-N_grid = (6, 6, 25)
-Qs = 32
-Ql = 25
+N_grid = (3, 3, 20)
+Qs = 25
+Ql = 18
 r_c = 1.799918355128706
-Q_0 = 50
-Rz_0 = 60
+Q_0 = 20
+Rz_0 = 20
 
 
 #for data in ["n_1000.jld2", "n_3164.jld2", "n_10000.jld2","n_31624.jld2","n_100000.jld2"]
@@ -49,6 +52,16 @@ for data in ["n_100000.jld2"]
 	@time energy_sog = ExTinyMD.energy(fssog_interaction, fssog_neighbor, info, atoms)
 
 
+	for i in 1:fssog_interaction.n_atoms
+		fssog_interaction.charge[i] = atoms[info.particle_info[i].id].charge
+		fssog_interaction.position[i] = info.particle_info[i].position.coo
+	end
+
+	t_short = @time energy_short(fssog_interaction, fssog_neighbor)
+	t_mid = @time energy_mid(fssog_interaction)
+	t_long = @time energy_long(fssog_interaction)
+
+	@show t_short,t_mid,t_long
 
 	abs_error = abs(energy_ewald - energy_sog)
 	relative_error = abs(energy_ewald - energy_sog) / abs(energy_ewald)
